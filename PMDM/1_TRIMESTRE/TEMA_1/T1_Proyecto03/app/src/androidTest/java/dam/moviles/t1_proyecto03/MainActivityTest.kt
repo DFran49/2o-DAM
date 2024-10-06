030f8f64127f492fa210cdc7ebf75ab8f57b9af0
DFran49 <francresmartin@gmail.com>
2024-10-07 00:17:56 +0200

    rucula

---
+++ PMDM/1_TRIMESTRE/TEMA_1/T1_Proyecto03/app/src/androidTest/java/dam/moviles/t1_proyecto03/MainActivityTest.kt
@@ -0,0 +1,17 @@
+package dam.moviles.t1_proyecto03
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+
+class MainActivityTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
